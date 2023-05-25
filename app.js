59a593b054afe85caa5b20f0874b84918a5adf68
Harshit <gaurharshit4@gmail.com>
2023-05-25 16:13:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const AppLayout = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+};
+
+root.render(<AppLayout />);
